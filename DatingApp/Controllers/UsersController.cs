d073b743a6e467ef9e5fc450b2c9c01067714be3
jayobekpa@gmail.com <jayobekpa@gmail.com>
2022-06-02 16:27:50 +0100

    section 1 completed

---
+++ DatingApp/Controllers/UsersController.cs
@@ -0,0 +1,38 @@
+﻿using DatingApp.Data;
+using DatingApp.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DatingApp.Controllers
+{
+    // user controller route
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsersController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public UsersController(DataContext context)
+        {
+            _context = context;
+        }
+
+        // End point to get a list of users from the database
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+        {
+           return await _context.Users.ToListAsync();
+        }
+
+        // End point to get a user by Id from the database
+        [HttpGet("{id}")]
+        public async Task<ActionResult<AppUser>> GetUser(int id)
+        {
+            return await _context.Users.FindAsync(id);
+        }
+    }
+}
---
+++ DatingApp/Data/DataContext.cs
@@ -0,0 +1,20 @@
+﻿using DatingApp.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DatingApp.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        // Dbset property for user to create a database
+        public DbSet<AppUser> Users { get; set; }
+    }
+}
